17ffbf9b815d316c88e0f1c825b7a3e073794505
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2021-07-01 00:33:02 +0100

    first commit with mocking rest

---
+++ .storybook/preview.js
@@ -0,0 +1,19 @@
+import { setCompodocJson } from "@storybook/addon-docs/angular";
+import docJson from "../documentation.json";
+import { addDecorator } from "@storybook/angular";
+import { initializeWorker, mswDecorator } from "msw-storybook-addon";
+setCompodocJson(docJson);
+
+initializeWorker();
+addDecorator(mswDecorator);
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  docs: { inlineStories: true },
+};
